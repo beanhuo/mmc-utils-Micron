45992bc2aa02e9007b36c66b0ede8c020226b953
Bean Huo <beanhuo@micron.com>
2024-10-22 18:58:49 +0200

    mmc-utils: Add FFU mode 5 for firmware download using repeated CMD24 single-block write command
    Add FFU mode 5 which enters FFU mode with CMD6, followed by repeated CMD24 commands to perform
    single-block writes for the firmware download. After downloading all firmware data, CMD6 is issued
    to exit FFU mode.
    
    Signed-off-by: Bean Huo <beanhuo@micron.com>
    Acked-by: Avri Altman <avri.altman@wdc.com>

--- mmc.c
+++ mmc.c
@@ -249,6 +249,11 @@ static struct Command commands[] = {
 	 "Same as 'ffu1', but uses CMD24 Single-block write to download, exiting FFU mode after each block written.\n",
 	 NULL
 	},
+	{ do_ffu5, -2,
+	 "ffu5", "<image name> <device> [chunk-bytes]\n"
+	 "Same as 'ffu1', but uses CMD24 Single-block write for repeated downloads, remaining in FFU mode until completion.\n",
+	 NULL
+	},
 	{ do_erase, -4,
 	"erase", "<type> " "<start address> " "<end address> " "<device>\n"
 		"Send Erase CMD38 with specific argument to the <device>\n\n"
